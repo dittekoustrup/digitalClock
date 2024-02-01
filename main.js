59b897e9ebfed932fd08cdcb4092f7bada6d65b3
dittekoustrup <dittekoustrup@outlook.com>
2024-02-01 14:20:47 +0100

    add real month

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function time() {
     clockElement.innerHTML = ("0" + h).substr(-2) + ":" + ("0" + m).substr(-2) + ":" + ("0" + s).substr(-2);
 
     const dateElement = document.getElementById("date");
-    dateElement.innerHTML = date.getDate() + "." + date.getMonth() + 1 + "." + date.getFullYear();
+    dateElement.innerHTML = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear();
 }
 
 setInterval(time, 1000);
